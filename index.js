fb4e22e4e61e52533ddcfb1441cdb209cd5caec0
AdarshG930 <itsadarshofficial1@gmail.com>
2023-01-20 21:27:20 +0530

    handling Asynchronous js using promises

--- index.js
+++ index.js
@@ -106,15 +106,52 @@
 // });
 
 ////////////////////////////////
-const fs = require('fs');
 
-const input = process.argv;
-if(input[2]=='add'){
-    fs.writeFileSync(input[3], input[4]);
-}
-else if(input[2]=='remove'){
-    fs.unlinkSync(input[3]);
-}
-else{
-    console.log('invalid output')
-}
+// const fs = require('fs');
+
+// const input = process.argv;
+// if(input[2]=='add'){
+//     fs.writeFileSync(input[3], input[4]);
+// }
+// else if(input[2]=='remove'){
+//     fs.unlinkSync(input[3]);
+// }
+// else{
+//     console.log('invalid output')
+// }
+
+////////////////////////////////
+
+// const fs = require('fs');
+// const path = require('path');
+
+// const dirPath = path.join(__dirname,'files/')
+
+// // for(let i=0;i<5;i++){
+// //     fs.writeFileSync(`${dirPath}hello${i}.txt`,"a simple text file")
+// // }
+
+// // fs.readdir(dirPath,(err,items)=>{
+// //     console.warn(items)
+// // });
+// // fs.readdir(dirPath,(err,items)=>{
+// //     items.forEach((item)=>{
+// //         console.log(item)
+// //     })
+// // });
+
+////////////////////////////////
+
+let a= 20;
+let b= 0;
+
+let waitingData = new Promise((resolve,reject)=>{
+    setTimeout(() => {
+      resolve(30);
+    }, 2000);
+})
+
+waitingData.then((data)=>{
+    b = data;
+  console.log(a+b)
+});
