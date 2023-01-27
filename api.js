eef163a0b766be8c6e7a05788ca790f99da41572
AdarshG930 <itsadarshofficial1@gmail.com>
2023-01-27 23:50:28 +0530

    inserted data from postman to mongodb using post api methode

--- api.js
+++ api.js
@@ -3,10 +3,18 @@ const app = express();
 const dbConnect = require('./mongodb');
 const port = 5200;
 
+app.use(express.json());
+
 app.get('/',async (req,res)=>{
     let data = await dbConnect();
     data = await data.find().toArray();
     res.send(data);
+}) 
+
+app.post('/',async(req,res)=>{
+    let data = await dbConnect();
+    let result = await data.insertOne(req.body)
+    res.send(result);
 })
 
 app.listen(port,()=>{
